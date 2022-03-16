00f7316255b04e19f2a63861c5e096c8740a77e1
Chrishan Jayakody <chrishan@cinnamonit.com>
2022-03-17 04:27:46 +0530

    Retrieved All the data for the analysing tweet

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const socketIO = require('socket.io')
 const needle = require('needle')
 const config = require('dotenv').config()
 const TOKEN = process.env.BEARER_TOKEN
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 
 const app = express()
 
@@ -18,9 +18,9 @@ app.get('/', (req, res) => {
 })
 
 const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules'
-const streamURL = 'https://api.twitter.com/2/tweets/search/stream?tweet.fields=created_at,public_metrics&expansions=author_id'
+const streamURL = 'https://api.twitter.com/2/tweets/search/stream?tweet.fields=author_id,referenced_tweets&expansions=referenced_tweets.id.author_id'
 
-
+//value: '@SathyapalaBot -is:reply'
 const rules = [{
   value: '@SathyapalaBot'
 }]
@@ -81,29 +81,84 @@ function streamTweets(socket) {
     }
   })
 
-  stream.on('data', (data) => {
+  stream.on('data', data => {
     try {
-      console.log(data)
-      //const json = JSON.parse(data)
-      //getTweetInfo(json.data.id)
+
+      const json = JSON.parse(data)
+      console.log(json.data)
+      getTweetInfo(json)
       //socket.emit('tweet', json)
     } catch (error) {
+      if (data.detail === "This stream is currently at the maximum allowed connection limit.") {
+        console.log(data.detail)
+        process.exit(1)
+      } else {
+        console.log("Catch Error: " + error)
+        console.log("Catch Error: " + data)
+      }
+    }
+  }).on('err', error => {
+    if (error.code !== 'ECONNRESET') {
+      console.log(error.code);
+      process.exit(1);
+    } else {
       console.log(error)
     }
-  })
+  });
 }
 
-async function getTweetInfo(id) {
+async function getTweetInfo(json) {
 
-  const reTweetedByURL = `https://api.twitter.com/2/tweets/${id}/retweeted_by`
+  // Author IDs for Training
+  // SathyapalaBot - 907886923678711809
+  // ChrishaJay - 2459987869
 
-  const response = await needle('get', reTweetedByURL, {
+  const text = json.data.text
+  console.log(text)
+  const requester = json.data.author_id
+  const refTweet = json.data.referenced_tweets[0]
+
+  const retweetedByURL = `https://api.twitter.com/2/tweets/${refTweet.id}/retweeted_by`
+  const likedURL = `https://api.twitter.com/2/tweets/${refTweet.id}/liking_users`
+  const tweetInfoURL = `https://api.twitter.com/2/tweets/${refTweet.id}?tweet.fields=author_id,public_metrics`
+
+  const retweetResponse = await needle('get', retweetedByURL, {
     headers: {
       Authorization: `Bearer ${TOKEN}`
     }
   })
 
-  console.log(response.body)
+  console.log("********** Retweet ***************")
+  console.log(retweetResponse.body)
+
+  const likeResponse = await needle('get', likedURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`
+    }
+  })
+
+  console.log("********** Like ***************")
+  console.log(likeResponse.body)
+
+  const infoResponse = await needle('get', tweetInfoURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`
+    }
+  })
+
+  console.log("********** Info ***************")
+  const tweetInfo = infoResponse.body.data
+  console.log(tweetInfo)
+
+  const authorInfoURL = `https://api.twitter.com/2/users/${tweetInfo.author_id}?user.fields=verified,username`
+  const authorInfoResponse = await needle('get', authorInfoURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`
+    }
+  })
+
+  console.log("********** Author ***************")
+  console.log(authorInfoResponse.body)
 }
 
 io.on('connection', async () => {
@@ -114,7 +169,7 @@ io.on('connection', async () => {
     // Get all stream rules
     currentRules = await getRules()
 
-    console.log(currentRules)
+    //console.log(currentRules)
     // Delete all stream rules
     await deleteRules(currentRules)
 
